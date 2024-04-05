dc58d5ebb26743659d860dc19a68e1047176d97c
Graham Matuszewski <matuszeg@gmail.com>
2024-04-05 16:47:33 -0400

    Update connectPIA.sh

--- connectPIA.sh
+++ connectPIA.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 if test -f /home/pia/login.txt; then
+  piactl background enable
+  piactl -u applysettings '{"killswitch":"on"}'
+  piactl set protocol wireguard --debug
+  piactl set allowlan true --debug
+  piactl set requestportforward true --debug
   piactl connect
 fi
